eb1664ffc8302fded675d24538882a2b863e0536
jero-r <g.rivoira@outlook.com>
2020-12-02 06:39:05 -0300

    little suprise on main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <ctime>
 #include <cstdlib>
 #include "Game.h"
+#define LOVIGAME RIVOGAME
 using namespace std;
+
 
 int main(int argc, char *argv[]) {
 	
 	srand(time(nullptr));
 	
-	Game LOVIGAME(1024, 840, "RIVOGAME");
-	LOVIGAME.Run();
+	Game LOVIGAME(1024, 840, "GABOGAME");
+	RIVOGAME.Run();
 	
 	return 0;
 }
