a7150665ba31517f062f5affcdf136dc672e1234
jero-r <g.rivoira@outlook.com>
2020-12-02 19:15:41 -0300

    now Entity is in charge of loading any entity texture

--- Entity.cpp
+++ Entity.cpp
@@ -1,6 +1,26 @@
 #include "Entity.h"
+#include "Settings.h"
 
 bool Entity::CollidesWith(const Entity & another) {
 	return m_sprite.getGlobalBounds().intersects(another.m_sprite.getGlobalBounds());
+}
+
+Entity::Entity (std::string keyword) : m_key(keyword) {
+	
+}
+
+void Entity::LoadTextures ( ) {
+	Settings s("textures.conf", m_key);
+	int psize = std::stoi(s[m_key+"-size"]);
+	
+	m_textures.resize(psize);
+	for (size_t i=0; i<m_textures.size(); ++i)
+		m_textures[i].loadFromFile(s[m_key+"-texture"+std::to_string(i)] + ".png");
+	
+	if (psize) m_sprite.setTexture(m_textures[0]);
+}
+
+void Entity::SetKeyword (std::string new_key) {
+	m_key = new_key;
 }
 
--- Entity.h
+++ Entity.h
@@ -6,7 +6,6 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Rect.hpp>
 #include <SFML/System/Vector2.hpp>
-#include "World.h"
 #include <vector>
 
 // * Entity es la clase madre de cualquier (valga la redundancia) entidad   
@@ -17,10 +16,16 @@ class Game;
 
 class Entity {
 public:
-	virtual void Update(World &m_world) = 0; // las entidades se actualizan segun el mundo en el que estan
+	virtual void Update() = 0; // las entidades se actualizan segun el mundo en el que estan
 	virtual void Draw(sf::RenderWindow &win) = 0;
+	void SetKeyword(std::string new_key);
 	bool CollidesWith(const Entity &another);
+	sf::Sprite &GetSprite() { return m_sprite; }
+	Entity(std::string keyword);
 protected:
+	void LoadTextures();
+	
+	std::string m_key;
 	sf::Vector2f m_speed;
 	float m_topspeed;
 	std::vector<sf::Texture> m_textures;
