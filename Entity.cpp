340742772b7631dd3eff3db26a9fb2f40da83319
jero-r <g.rivoira@outlook.com>
2020-12-02 06:34:52 -0300

    thanks to Settings, saving spritenames is no longer needed

--- Entity.cpp
+++ Entity.cpp
@@ -1,12 +1,8 @@
 #include "Entity.h"
 
-Entity::Entity(std::string spritename, float initial_x, float initial_y) : 
-	m_spritename(spritename), m_speed(0, 0)
+Entity::Entity(float initial_x, float initial_y) : m_speed(0, 0)
 {
-	m_textures.resize(1);
-	m_textures[0].loadFromFile(m_spritename);
-	m_sprite.setTexture(m_textures[0]);
-	m_sprite.setPosition(initial_x, initial_y);
+
 }
 
 bool Entity::CollidesWith(const Entity & another) {
--- Entity.h
+++ Entity.h
@@ -20,10 +20,10 @@ public:
 	virtual void Update(World &m_world) = 0; // las entidades se actualizan segun el mundo en el que estan
 	virtual void Draw(sf::RenderWindow &win) = 0;
 	bool CollidesWith(const Entity &another);
-	Entity(std::string spritename, float initial_x, float initial_y);
+	Entity(float initial_x, float initial_y);
 protected:
-	std::string m_spritename;
 	sf::Vector2f m_speed;
+	float m_topspeed;
 	std::vector<sf::Texture> m_textures;
 	sf::Sprite m_sprite;
 };
