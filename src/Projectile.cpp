c84ce94ff54433140edbf302c77ae549ccf2bee0
jero-r <g.rivoira@outlook.com>
2021-01-07 09:32:11 -0300

    now Projectile is purely virtual
    not fully; game would crash if Shovel couldn't generate a standard projectile, will add MeleeProjectile in the future nonetheless

--- src/Projectile.cpp
+++ src/Projectile.cpp
@@ -3,17 +3,3 @@
 Projectile::Projectile(sf::Vector2f pos, std::string keyword) : 
 	Entity(pos, keyword)
 { }
-
-//void Projectile::ApplyEffect (Player * target) {
-//	target->GetSprite().setPosition(target->GetInitPos());
-//	target->SetSpeed({0, 0});
-//}
-
-void Projectile::Update ( ) {
-	m_sprite.move(-15, 0);
-}
-
-void Projectile::Draw (sf::RenderWindow & win) {
-	win.draw(m_sprite);
-}
-
--- src/Projectile.h
+++ src/Projectile.h
@@ -1,17 +1,17 @@
 #ifndef PROJECTILE_H
 #define PROJECTILE_H
 #include "Entity/Entity.h"
+#include "Entity/Player.h"
 
 class Player;
 
 class Projectile : public Entity {
 public:
 	Projectile(sf::Vector2f pos, std::string keyword);
-	void Update() override;
-	void Draw(sf::RenderWindow &win) override;
-//	virtual void ApplyEffect(Player *target);
-private:
-	bool in_use;
+	bool IsUsed() const { return in_use; }
+	virtual void ApplyEffect(Player *target) = 0;
+protected:
+	bool in_use = true;
 };
 
 #endif
