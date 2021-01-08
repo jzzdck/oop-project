a691dfefa86f46d6e94f27234da5a34522319675
jero-r <g.rivoira@outlook.com>
2021-01-08 10:21:19 -0300

    more polished revolver

--- src/Entity/Item/Weapon/Revolver.cpp
+++ src/Entity/Item/Weapon/Revolver.cpp
@@ -3,7 +3,7 @@
 #include "../../../Bullet.h"
 
 Revolver::Revolver(sf::Vector2f pos, bool facing, float wdt) : 
-	Weapon(pos, "revolver", 30, facing, wdt)
+	Weapon(pos, "revolver", 30, facing, wdt), is_shooting(false)
 { m_sprite.scale(2,2); }
 
 void Revolver::Draw(sf::RenderWindow & win) {
@@ -14,6 +14,16 @@ void Revolver::Draw(sf::RenderWindow & win) {
 Projectile * Revolver::GetProjectile ( ) {
 	auto pos = m_sprite.getPosition();
 	float dir = m_current ? -1.f : 1.f;
-	return new Bullet(dir*50, pos);
+	pos.y += 3;
+	return new Bullet(dir*15, pos);
+}
+
+bool Revolver::IsAttacking ( ) {
+	if (is_shooting != attack_state) {
+		is_shooting = !is_shooting;
+		return is_shooting;
+	}
+	
+	return false;
 }
 
