589f6f416e98017b1ef09ea9f5787ef739ccbd84
jero-r <g.rivoira@outlook.com>
2021-01-13 09:24:01 -0300

    rotation is mirrored, projectile is gen'd at cannon's top
    negative scaling when sprite is facing oposite direction, instead of having another texture

--- src/Entity/Item/Weapon/Handcannon.cpp
+++ src/Entity/Item/Weapon/Handcannon.cpp
@@ -8,49 +8,49 @@ Handcannon::Handcannon(sf::Vector2f pos, bool facing) :
 	Weapon(pos, "handcannon", 30, !facing, 20)
 {
 	m_sprite.scale(2,2);
+	
+	m_sprite.setTexture(m_textures[!m_current]);
 }
 
 void Handcannon::Draw(sf::RenderWindow & win) {
-	auto dim = m_sprite.getLocalBounds();
-	float dir = 1.f;
+	float dir = m_current ? 1.f : -1.f;
 	
-	m_sprite.setTexture(m_textures[!m_current]);
-	if (!m_current) dir = -1.f, dim.width = 0;
-	m_sprite.rotate(dir*float(m_angle*180/M_PI));
-	m_sprite.setOrigin(dim.width, dim.height);
+	m_sprite.setRotation(dir*m_angle*180.f/M_PI);
+	m_sprite.setScale(-dir*2, 2);
+	if (Owner() != -1 && m_current)
+		m_sprite.move(m_sprite.getGlobalBounds().width, 0);
 	
-	if (Owner() != -1) 
-		m_sprite.move(dim.width, dim.height);
 	win.draw(m_sprite);
-	
-	m_sprite.setRotation(0);
 }
 
 bool Handcannon::IsAttacking ( ) {
 	if (m_nextangle) m_angle = m_nextangle = 0;
 	
-	if (attack_state) {
-		m_angle += 0.025;
-		m_angle = std::min(m_angle, float(M_PI/2.f));
-	}
+	if (attack_state)
+		m_angle += 0.025, m_angle = std::min(m_angle, float(M_PI)/2.f);
 	
 	if (attack_state != is_shooting) {
 		is_shooting = !is_shooting;
 		bool can_shoot = firerate.getElapsedTime().asSeconds() > 0.5f;
-		if (!is_shooting && can_shoot) { 
-			m_nextangle = m_angle;
-			firerate.restart();
-		}
+		if (!is_shooting && can_shoot) 
+			m_nextangle = m_angle, firerate.restart();
 		
 		return !is_shooting && can_shoot;
 	} else return false;
 }
 
 Projectile * Handcannon::GetProjectile ( ) {
-	float dir = m_current ? -1.f : 1;
-	auto pos = m_sprite.getPosition();
-	pos.x += dir*20;
+	float dir = m_current ? -1.f : 1.f;
+	auto globals = m_sprite.getGlobalBounds();
 	
-	return new Bomb({dir*15*std::cos(m_angle), -20*std::sin(m_angle)}, pos);
+	sf::Vector2f pos = { 
+		globals.left + globals.width * (dir == 1.f ? .5f : 1.f),
+		globals.top + 4
+	};
+	
+	return new Bomb({ 
+		m_current ? -1.f : 1.f *15*std::cos(m_angle), 
+		-20*std::sin(m_angle)
+	}, pos);
 }
 
