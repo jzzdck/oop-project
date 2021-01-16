31e3331c4bdebb93c1f8967dc3cc86827cd9de1d
jero-r <g.rivoira@outlook.com>
2021-01-16 10:58:19 -0300

    proyectiles now have damage points

--- src/Entity/proyectile/Projectile.h
+++ src/Entity/proyectile/Projectile.h
@@ -8,7 +8,7 @@ class Player;
 
 class Projectile : public Entity {
 public:
-	Projectile(sf::Vector2f pos, std::string keyword);
+	Projectile(sf::Vector2f pos, std::string keyword, float m_damage);
 	bool IsUsed() const { return in_use; }
 	virtual void Draw(sf::RenderWindow &win) { 
 		utils::flipTexture(m_dir, m_scale, m_sprite);
@@ -18,6 +18,7 @@ public:
 	virtual void ApplyEffect(Player *target);
 protected:
 	bool in_use = true;
+	float m_damage;
 };
 
 #endif
