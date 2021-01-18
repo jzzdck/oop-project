df938366df1f3662e74d322e375c15f7b18bb127
jero <jzzdck@yahoo.com>
2021-01-18 16:31:52 -0300

    working healthbar

--- src/HUD.cpp
+++ src/HUD.cpp
@@ -4,6 +4,12 @@
 HUD::HUD (float dir, const sf::Vector2f &pos) : 
 	m_dir(dir), m_pos(pos) 
 {
+	m_healthbar.resize(3); 
+	for(size_t i=0;i<m_healthbar.size();i++) { 
+		m_healthbar[i].setSize({210.f, 21.f});
+		m_healthbar[i].setFillColor({169, 59, 59});
+	}
+	
 	m_texture.loadFromFile("res/healthbar.png");
 	m_hud.setTexture(m_texture);
 	m_hud.setPosition(m_pos);
@@ -22,13 +28,21 @@ void HUD::Draw (sf::RenderWindow & win, float zoom_level) {
 	m_playercolor.setScale(m_dir*zoom_level, 1.f*zoom_level);
 	m_hud.setScale(m_dir*3*zoom_level, 3*zoom_level);
 	
+	pos = win.mapPixelToCoords(sf::Vector2i(m_pos.x + m_dir*7*3, m_pos.y + 3*20));
+	m_healthbar[0].setPosition(pos);
+	m_healthbar[0].setScale(m_dir*zoom_level, zoom_level);
+	
+	win.draw(m_healthbar[0]);
 	win.draw(m_playercolor);
 	win.draw(m_hud);
 }
 
 void HUD::Update ( ) {
+	float health = m_target->GetHealth();
+	if (health <= 0.f) health = 0.f;
+	m_healthbar[0].setSize({health/1000.f * 210.f, 21.f});
 }
 
 void HUD::SetPlayer (const Player * target) {
-	
+	m_target = target;
 }
