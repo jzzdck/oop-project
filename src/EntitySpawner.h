b7a2027dcf8a9d455b75752b336babc98a82dfe4
jero-r <g.rivoira@outlook.com>
2021-01-20 10:12:48 -0300

    added spawner class for entities
    so EntitiesFacade doesnt spawn entities itself, the responsibility is delegated

---
+++ src/EntitySpawner.h
@@ -0,0 +1,23 @@
+#ifndef ENTITYSPAWNER_H
+#define ENTITYSPAWNER_H
+#include "Entity/Player.h"
+#include "Entity/Item/Item.h"
+#include "Entity/Item/Flag.h"
+#include "Entity/Item/Weapon/Shovel.h"
+#include "Entity/Item/Weapon/Revolver.h"
+#include "Entity/Item/Weapon/Handcannon.h"
+
+class EntitySpawner {
+public:
+	EntitySpawner		 (const sf::Vector2f &winsize);
+	Player * SpawnPlayer (const sf::Vector2f &pos, int player_index);
+	Flag   * SpawnFlag   (const sf::Vector2f &pos, int player_index);
+	Weapon * SpawnWeapon (const sf::Vector2f &pos, int        index);
+	
+	Item   * RandomItem();
+	Weapon * RandomWeapon();
+private:
+	sf::Vector2f m_winsize;
+};
+
+#endif
