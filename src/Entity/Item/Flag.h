3bdc305a588d1657928e7c4487d1c0fa22a55f79
jero-r <g.rivoira@outlook.com>
2021-01-22 10:06:52 -0300

    added logic for checking previous owner and who the flag belongs to

--- src/Entity/Item/Flag.h
+++ src/Entity/Item/Flag.h
@@ -8,7 +8,14 @@ public:
 	Flag(sf::Vector2f pos, bool who);
 	void Draw(sf::RenderWindow & win) override;
 	void Update() override;
+	
+	bool IsTheFlag() const override { return true; }
+	void SetOwner(int owner) override;
+	int PreviousOwner() const override { return prev_owner; }
+	int BelongsTo() const override { return m_who; }
 private:
+	int prev_owner = -1;
+	int m_who;
 	sf::Sprite m_dep;
 	Trail m_trail;
 };
--- src/Entity/Item/Item.h
+++ src/Entity/Item/Item.h
@@ -6,9 +6,13 @@ class Item : public Entity {
 public:
 	Item(sf::Vector2f pos, std::string keyword);
 	void ApplyResponse(const sf::Vector2f &vec) override;
+	void SetUsing(bool use) { in_use = use; }
 	
-	void SetOwner(int owner) { m_owner = owner; }
 	int Owner() const { return m_owner; }
+	virtual void SetOwner(int owner) { m_owner = owner; }
+	virtual int PreviousOwner() const { return -2; }
+	virtual bool IsTheFlag() const { return false; }
+	virtual int BelongsTo() const { return -1; }
 	
 	void Update() override;
 	virtual void Draw(sf::RenderWindow & win);
