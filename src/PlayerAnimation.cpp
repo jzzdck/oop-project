99afab5a7ccf5cfdbbec2340130f0258706526f5
jero-r <g.rivoira@outlook.com>
2021-02-17 09:25:48 -0300

    minor tweaks

--- src/PlayerAnimation.cpp
+++ src/PlayerAnimation.cpp
@@ -3,10 +3,10 @@
 PlayerAnimation::PlayerAnimation(sf::Sprite * target, sf::Sprite * indep) {
 	m_animations = {
 		{target, indep, "running", {8, 0, 0.06f, {42.f, 50.f}}},
-		{target, indep, "jump", {4, 0, 0.05f, {120.f/4.f, 50.f}}},
+		{target, indep, "jump", {4, 0, 0.1f, {120.f/4.f, 50.f}}},
 		{target, indep, "idle", {4, 0, 0.08f, {22.f, 50.f}}},
 		{target, indep, "impact", {4, 0, 0.06f, {108.f/4.f, 45.f}}},
-		{target, indep, "death", {4, 0, 0.15f, {248.f/4.f, 50.f}}}
+		{target, indep, "death", {4, 0, 0.1f, {248.f/4.f, 50.f}}}
 	};
 	
 	m_animations.at(1).SetLoopability(false);
@@ -15,7 +15,7 @@ PlayerAnimation::PlayerAnimation(sf::Sprite * target, sf::Sprite * indep) {
 }
 
 void PlayerAnimation::Update ( int current_state ) {
-	if (previous != current_state && (previous == 3 or previous == 1))
+	if (previous != current_state && (previous == 3 or previous == 1 or previous == 4))
 		m_animations.at(previous).Reset();
 	
 	m_animations.at(current_state).Update();
